339806265b2b419769ad04d5d5ab088f67c679b2
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2023-09-07 11:32:50 -0300

    admin js configuration

--- cloneflix_api/src/server.ts
+++ cloneflix_api/src/server.ts
@@ -1,12 +1,16 @@
 import express from "express";
-import {sequelize} from "./database"
+import {database} from "./database"
+import {adminJs, adminJsRouter} from "./adminjs"
 
 const app = express();
+
+app.use(express.static("public"));
+app.use(adminJs.options.rootPath, adminJsRouter)
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
-    sequelize.authenticate().then(()=>{
+    database.authenticate().then(()=>{
         console.log("Database connection successfull")
     }).catch((error)=>{
         console.log(`Error connecting to database ${error}`);
