eed365fa324c8ac7956f1795ccc4006d818bf3fe
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2023-09-14 20:01:51 -0300

    Creation of migration and movie model, and relationship of movie  and category tables

--- cloneflix_api/src/adminjs/resources/index.ts
+++ cloneflix_api/src/adminjs/resources/index.ts
@@ -1,10 +1,15 @@
 import { ResourceWithOptions } from "adminjs";
-import { Category } from "../../models";
+import { Category, Movie } from "../../models";
 import { categoryResourceOptions } from "./category";
+import { movieResourceOptions } from "./movie";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Category,
     options: categoryResourceOptions
+  },
+  {
+    resource: Movie,
+    options: movieResourceOptions
   }
 ]
